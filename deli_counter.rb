550bf0904e389d5e413db12d0e3a77998d7bef61
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-14 16:11:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,9 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else 
-    katz_deli.each.with_index(1) do |name, index|
-      puts "The line is currently: #{index}.#{name}."
+    string_var = "The line is currently:"
+    katz_deli.each.with.index do |name, index|
+    string_var << "#{index+1}. #{name}"
     end 
     
   #   katz_deli.each_with_index do |index,name| 
