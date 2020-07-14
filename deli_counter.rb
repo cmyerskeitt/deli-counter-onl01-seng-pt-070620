4ef03afc3868c7b371a1a21c891461cf86934802
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-14 16:35:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,10 @@ def take_a_number(katz_deli, name)
     katz_deli << name 
       puts "Welcome, #{name}. You are number #{katz_deli.length} in line."  
 end 
+
+def now_serving 
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+    
+  else 
+    string_var = "Currently Serving #{katz_deli{"
